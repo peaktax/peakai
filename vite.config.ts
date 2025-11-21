2cd962c25cc3a01a2765ecf59b0b33ebca899fce
peaktax <info@ourtaxpartner.com>
2025-11-21 14:06:25 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
-  const env = loadEnv(mode, (process as any).cwd(), '');
+  const env = loadEnv(mode, '.', '');
 
   return {
     plugins: [react()],
-    // Define global constants replacement to support process.env.API_KEY style access
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.VITE_API_KEY),
-      'process.env.VITE_APP_PASSWORD': JSON.stringify(env.VITE_APP_PASSWORD),
-    },
     build: {
       outDir: 'dist',
     },
